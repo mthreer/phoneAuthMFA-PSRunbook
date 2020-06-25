692a243144f464a32a2db1632576642021ad3e1d
Niklas Jumlin <mthreer@gmail.com>
2020-06-25 09:32:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,116 @@
+# phoneAuthMFA-PSRunbook
+## Managing of phoneNumber for multi-factor auth (MFA) from a Automation Runbook towards Microsoft Graph API
+
+## The problem
+
+Lets say you have a customer / tenant with the requirement that MFA and SSPR should only be registered when connected on the office network or via VPN, as measure to prevent identity theft via unsecure passwords for users yet not enrolled with MFA.
+
+Hence, the following conditional access rules apply, together with combined-registration being enabled:\
+https://docs.microsoft.com/en-us/azure/active-directory/authentication/howto-registration-mfa-sspr-combined#conditional-access-policies-for-combined-registration
+
+During the current COVID-19 pandemic, this has caused problems for users working from home and thus there has been a need to centrally manage the phone number used for MFA for users in Azure AD.
+
+Historically, there has been no sure way to programmatically manage MFA registration for users. However, it has long been possible to manage the data via graphical GUI in Azure AD per user. However, this is a laborious task to do manually and introduces the risk of making mistakes when handling multiple users.
+
+Sure, you can sync the **mobile** attribute from AD, which will then be used as a suggestion for when the user first logs in and has to register MFA, but the user still has the final power to choose another phone number if desired and the users also has to complete the MFA-registration process themself, and if using combined registration, also SSPR.
+
+## The solution
+
+The following user feedback / voice received response from Microsoft, on May 28, 2020, that they had released features to the Graph API that enables management of phone numbers for MFA:\
+[Azure Feedback: PowerShell and Graph API support for managing Multi-Factor Authentication](https://feedback.azure.com/forums/169401-azure-active-directory/suggestions/20249953-powershell-and-graph-api-support-for-managing-mult)
+
+This solves the problem of previously not being able to programmatically manage the MFA data on behalf of the users. 
+We can now via the attribute values (in my case, **extensionAttribute15** from AD) from IAM / directory services control which phone number the users should have configured for multifactor authentication (MFA).
+
+The users can then log in and configure their MFA themselves to use the Authenticator app for example - which is the recommended method for login. However, by forcing which phone number the user receives MFA-challenges to, staff and administrators can be more safe knowing that the user logging in is the right person.
+
+## Technical solution for authenticating to Azure via Graph API:
+
+From the documentation from Microsoft, to be able to handle phoneAuthenticationMethod via Graph, we can see that this can only be done via Delegated Permissions:\
+https://docs.microsoft.com/en-us/graph/api/phoneauthenticationmethod-update?view=graph-rest-beta&tabs=http
+
+![Permissions required](/images/api_permissions.PNG)
+
+Based on the above information, it appears that authentication must be done via a "service account" with a username and password, and must be given an appropriate role, in my case the **Authentication Admin** role was sufficient in order to have the necessary permissions to configure MFA on behalf of the users. We can also see that the Enterprise App, that handles authentication, must be given the necessary rights (**UserAuthenticationMethod.ReadWrite.All**) of the type Delegated Permissions, it must also be able to read the basic data of users such as UserPrincipalName etc.
+
+![App_API_permissions](/images/App_permissions.PNG)
+
+The rest of the App registration was done with the following configuration:
+
+![Client_Secret](/images/app_clientsecret.PNG)
+
+![Redirect_Purposes](/images/app_redirect_purposes.PNG)
+
+Because this task requires a user account (service account) acting on behalf of other users, then this means that a so-called ROPC flow must be built for authenticating to MS Graph API:\
+https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth-ropc
+
+A ROPC (Resource Owner Password Credentials) flow means, involves authentication based on username, password, Application ID and Client Secret to Graph. For this reason, _**special measures should be taken to protect the data used for authentication**_.
+
+![ROPC](/images/ropc.PNG)
+
+## My actions to secure these credentials resulted in the following solutions
+
+In order to secure the use of these credentials and sensitive data, Azure is a suitable place to put this solution. Let me explain why:
+
+In Azure there is something called Runbooks and Automation Accounts.\
+https://docs.microsoft.com/en-us/azure/automation/automation-runbook-execution
+
+Pricing is per Job run time:\
+https://azure.microsoft.com/en-us/pricing/details/automation/
+
+Runbooks are serverless scripts that can be scheduled, triggered and used to automate a lot of things. Automation Accounts is what links Runbooks (script) with resources such as login information, accounts, certificates, files and modules etc.
+
+The following illustration shows the different input methods, the steps and the flow of how this is connected:
+
+![runbooks-architecture](/images/runbooks-architecture.png)
+
+One way to protect the credentials for the service account as well as the Client_Secret for the app is to put these parts of the script (authentication) in a Automation Account in Azure:
+
+![secure assets](/images/secureassets.PNG)
+
+* The Client_Secret can be placed in an encrypted variable (resource object) that can only be read from the specific Azure Automation Account where the runbook is running:\
+https://docs.microsoft.com/en-us/azure/automation/shared-resources/variables
+
+* Usernames and passwords can be entered into a resource object called Credentials, where they are stored encrypted and can only be read from the specific Azure Automation Account where the runbook is running:\
+https://docs.microsoft.com/en-us/azure/automation/shared-resources/credentials
+
+* Finally, in order to protect the account from unauthorized logins (if the login information somehow would have been leaked), we can limit from which locations (IP addresses) the account may log in from. I solved this with the help of Conditional Access and Named Locations, where the Azure Datacenter IP addresses located in North Europe (the same region that was chosen for the Resource Group in Azure for the Automation Account) were inserted into a Named Location and which were then allowed / exempted using Conditonal Access.    
+[Download: Azure IP Ranges and Service Tags – Public Cloud](https://www.microsoft.com/en-us/download/details.aspx?id=56519) (P.S I managed to get successful results with AzureCloud.NorthEurope from the file.)
+
+## The code
+
+After having done all the above, we can finally start implementing the logical steps involved in setting the phoneNumber for MFA using PowerShell. The following PowerShell Runbook is in place and doesnt really contain any sensitive information that on its own can be used get access to the tenat, there is a "handshake" which protects the execution of the runbook, but on its own, it will not give access to anything else in the tenant.
+
+[PowerShell Runbook Sourcecode](Runbook.ps1)
+
+As I mentioned above, I also added a "handshake" to further protect the execution of the script. 
+So dont forget to edit this line to set your own "secret" handshake:
+
+```powershell
+if ($WebhookData.RequestHeader.message -eq 'Handshake-message-only-known-between-runbook-and-invoker')
+```
+
+Then, to externally invoke the Runbook (script) contained in Azure Automation, you can use Webhooks that you set up for each Runbook, that will execute the runbook when RESTfully(?) called upon.\
+https://docs.microsoft.com/en-us/azure/automation/automation-webhooks
+
+See the example below for calling a REST API / Webhook (via PowerShell). The following would trigger the Runbook to execute, which logically translates into a request to check provided users extensionAttribute15 and phoneAuthenticationMethod and update them if necessary.
+
+```PowerShell
+$uri = "Your Webhook-URI which should be considered a secret"
+$users = @(
+           @{ UserPrincipalName="firstname1.lastname1@domain.com"},
+           @{ UserPrincipalName="firstname1.lastname1@domain.com"}
+)
+$body = ConvertTo-Json -InputObject $users
+$header = @{ Message = "Handshake-message-only-known-between-runbook-and-invoker" }
+$request = Invoke-WebRequest -Method Post -Uri $uri -Body $body -Headers $header
+```
+
+As the invoker, dont forget to also edit this line to match your "secret" handshake required by the runbook:
+
+```powershell
+$header = @{ Message = "Handshake-message-only-known-between-runbook-and-invoker" }
+```
+
+Job done! :smirk:
+
---
+++ Runbook.ps1
@@ -0,0 +1,221 @@
+﻿<#--------------------------------------
+
+Author: Niklas Jumlin (niklas.jumlin@atea.se)
+Version: 2020-06-24-1
+
+The script (software) reads a synchronized attribute value and updates or adds the value to the 
+phoneNumber used by Azure Multi-factor Authentication via Microsoft Graph API.
+
+Copyright (C) 2020 Niklas jumlin
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+-----------------------------------------#>
+
+Param
+(
+    [Parameter (Mandatory= $false)]
+    [Object]$WebhookData
+)
+
+# If runbook was called from Webhook, WebhookData will not be null.
+if ($WebhookData) {
+    Write-Output $WebhookData
+    
+    # Check header for message to validate request
+    if ($WebhookData.RequestHeader.message -eq 'Handshake-message-only-known-between-runbook-and-invoker')
+    {
+        Write-Output "Header has required information"}
+    else
+    {
+        Write-Output "Header missing required information";
+        exit;
+    }
+    # Special case for testing of data via test pane
+    if (-not($WebhookData.RequestBody)) {
+        $WebhookData = (ConvertFrom-Json -InputObject $WebhookData)
+        Write-Output "test $WebhookData"
+    }
+    $Users = (ConvertFrom-Json -InputObject $WebhookData.RequestBody)
+}
+
+# Connect to Azure Automation
+$Credentials = Get-AutomationPSCredential -Name 'Name of Credentials-resource object in the Automation Account'
+
+# This function converts a given PSObject to a HashTable recursively.
+# Graph returns JSON data as PSObjects. Hashtables are needed to achieve key to value pairing.
+function ConvertPSObjectToHashtable {
+    param (
+        [Parameter(ValueFromPipeline)]
+        $InputObject
+    )
+    process {
+        if ($null -eq $InputObject) { return $null }
+        if ($InputObject -is [System.Collections.IEnumerable] -and $InputObject -isnot [string]) {
+            $collection = @(
+                foreach ($object in $InputObject) { ConvertPSObjectToHashtable $object }
+            )
+            Write-Output -NoEnumerate $collection
+        }
+        elseif ($InputObject -is [psobject]) {
+            $hash = @{}
+            foreach ($property in $InputObject.PSObject.Properties) {
+                $hash[$property.Name] = ConvertPSObjectToHashtable $property.Value
+            }
+            $hash
+        }
+        else {
+            $InputObject
+        }
+    }
+}
+
+#$VerbosePreference = "Continue"
+
+#This is the ClientID (Application ID) of the registered Enterprise App in Azure AD
+$ClientID = "3965db2e-1dff-428c-8ce1-79bdb537b91e"
+
+#This is your Office 365 Tenant Domain Name or Tenant Id
+#$TenantId = "jumlin.onmicrosoft.com"
+$TenantId = "3619ea90-fa6e-40bf-aa11-2d4a18ad4521"
+
+$ReqTokenBody = @{
+    Client_Id       =   $ClientID
+    Client_Secret   =   Get-AutomationVariable -Name ClientSecret #This is the Client_Secret of the registered Enterprise app in Azure AD
+    Grant_Type      =   "Password"
+    Scope           =   "https://graph.microsoft.com/.default"
+    Username        =   $Credentials.Username # The username provided to the Credentials-resource object in the Automation Account
+    Password        =   [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Credentials.Password)) # The password provided to the Credentials-resource object in the Automation Account
+}
+Try {
+    $OAuthReq = Invoke-RestMethod -Method POST -Uri https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token -Body $ReqTokenBody -ContentType application/x-www-form-urlencoded -ErrorAction Stop
+    $TokenType = $OAuthReq.token_type
+    $AccessToken = $OAuthReq.access_token
+    Write-Output "Auth success! `'$TokenType`' AccessToken retrieved" 
+}
+Catch {
+    if ($_.ErrorDetails.Message) {
+        Write-Error "$((($_.ErrorDetails.Message | ConvertFrom-Json).error_description -split "`n")[0])" 
+    }
+    else {
+        Write-Error "$($_.CategoryInfo.Reason): $($_.Exception.Message)" 
+    }
+    Break;
+}
+foreach ($User in $Users) {
+    $UserPrincipalName = $User.UserPrincipalName
+    # Retrieve user data with synchronized value from extensionAttribute15
+    Try {
+        $apiUrl = "https://graph.microsoft.com/beta/users?`$filter=UserPrincipalName eq '$UserPrincipalName'&`$select=onPremisesExtensionAttributes,mail,UserPrincipalName"
+        $userAttributes = Invoke-RestMethod -Headers @{Authorization = "Bearer $AccessToken"} -Uri $apiUrl -Method GET -ErrorAction Stop
+        if ($userAttributes.value) {
+            Write-Output "Attributes for `'$($userAttributes.value.UserPrincipalName)`' was retrieved" 
+            Write-Output ("{0,-25}:{1,25}" -f 'extensionAttribute15',$($userAttributes.value.onPremisesExtensionAttributes.extensionAttribute15)) 
+            $NewMFA = @{
+                phoneNumber = "$($userAttributes.value.onPremisesExtensionAttributes.extensionAttribute15)"
+                phoneType   = "mobile"
+            }
+        }
+        if (-not($userAttributes.value)) {
+            throw "The query did not return any data for `'$UserPrincipalName`'"
+        }
+        if (-not($NewMFA.phoneNumber)) {
+            throw "The query did not return any data for extensionAttribute15 for user `'$UserPrincipalName`'"
+        }
+    }
+    Catch {
+        if ($_.ErrorDetails.Message) {
+            Write-Output "$((($_.ErrorDetails.Message | ConvertFrom-Json).error.message -split "`n")[0])" 
+        }
+        else {
+            Write-Output "$($_.CategoryInfo.Reason): $($_.Exception.Message)" 
+        }
+        Continue; # break loop
+    }
+    # Retrieve current authentication methods and filter for mobile type
+    Try {   
+        $apiUrl = "https://graph.microsoft.com/beta/users/$($userAttributes.value.UserPrincipalName)/authentication/methods"
+        $Methods = Invoke-RestMethod -Headers @{Authorization = "Bearer $AccessToken"} -Uri $apiUrl -Method GET -ErrorAction Stop
+        if ($Methods.value) {
+            $HashMethods = $Methods | ConvertPSObjectToHashtable
+            $CurrentMFA = ($HashMethods.value).Where{$_.PhoneType -eq 'mobile'}.phoneNumber
+            Write-Output ("{0,-25}:{1,25}" -f 'MFA phoneNumber',$CurrentMFA) 
+        }
+        if (-not($Methods.value)) {
+            throw "The query did not return any AuthenticationMethods for `'$UserPrincipalName`'"
+        }
+    }
+    Catch {
+        if ($_.ErrorDetails.Message) {
+            Write-Output "$((($_.ErrorDetails.Message | ConvertFrom-Json).error.message -split "`n")[0])" 
+        }
+        else {
+            Write-Output "$($_.CategoryInfo.Reason): $($_.Exception.Message)" 
+        }
+        Continue; # break loop
+    }
+    #
+    # Begin validation and updating/adding of new phoneNumber
+    #
+    if (($NewMFA.phoneNumber) -and ($CurrentMFA)) {
+        # Since we only receive the last 2 digits of the phoneNumber used for authentication from MS Graph, lets compare these last two digits with the one from extensionAttribute15
+        if ( $(($NewMFA.phoneNumber).Substring(($NewMFA.phoneNumber.Length)-2,2)) -eq $CurrentMFA.Substring(($CurrentMFA.Length)-2,2) ) {
+            Write-Output "Current MFA phoneNumber seems to match the value from extensionAttribute15" 
+        }
+        # phoneNumber does not match extensionAttribute15, lets update that
+        else {
+            Write-Warning "The user has a phoneNumber, but it does NOT seem to match the value from extensionAttribute15." 
+            Try {
+                # PUT (Update) phoneNumber using value from extensionAttribute15 as source
+                Write-Output "Updating phoneNumber with `'$($NewMFA.phoneNumber)`' - to match extensionAttribute15 . . ." 
+                $apiUrl = "https://graph.microsoft.com/beta/users/$($userAttributes.value.UserPrincipalName)/authentication/phoneMethods/$(($HashMethods.value).Where{$_.PhoneType -eq 'mobile'}.Id)"
+                $body = $NewMFA | ConvertTo-JSON
+                $null = Invoke-RestMethod -Headers @{Authorization = "Bearer $AccessToken"} -Body $body -ContentType application/json -Uri $apiUrl -Method PUT -ErrorAction Stop
+                Write-Output "The phoneNumber for $($userAttributes.value.UserPrincipalName) was successfully updated!" 
+            }
+            Catch {
+                if ($_.ErrorDetails.Message) {
+                    Write-Output "$((($_.ErrorDetails.Message | ConvertFrom-Json).error.message -split "`n")[0])" 
+                }
+                else {
+                    Write-Output "$($_.CategoryInfo.Reason): $($_.Exception.Message)" 
+                }
+                Continue; # break loop
+            }
+        }
+    }
+    # If there is currently no phoneNumber configured for MFA:
+    if (-not($CurrentMFA)) {
+        Write-Output "The user `'$($userAttributes.value.UserPrincipalName)`' does not have any mobile phoneNumber for MFA" 
+        if ($NewMFA.phoneNumber) {
+            Try {
+                # PUT (Update) phoneNumber using value from extensionAttribute15 as source
+                Write-Output "Adding phoneNumber: $($NewMFA.phoneNumber) . . ." 
+                $apiUrl = "https://graph.microsoft.com/beta/users/$($userAttributes.value.UserPrincipalName)/authentication/phoneMethods"
+                $body = $NewMFA | ConvertTo-JSON
+                $null = Invoke-RestMethod -Headers @{Authorization = "Bearer $AccessToken"} -Body $body -ContentType application/json -Uri $apiUrl -Method POST -ErrorAction Stop
+                Write-Output "A phoneNumber for $($userAttributes.value.UserPrincipalName) was successfully added!" 
+            }
+            Catch {
+                if ($_.ErrorDetails.Message) {
+                    Write-Output "$((($_.ErrorDetails.Message | ConvertFrom-Json).error.message -split "`n")[0])" 
+                }
+                else {
+                    Write-Output "$($_.CategoryInfo.Reason): $($_.Exception.Message)" 
+                }
+                Continue; # break loop
+            }
+        }
+    }
+} # end foreach user
